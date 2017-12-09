98dec8d036315213deb0fe79c600dce46488a1d8
Leonardo <lvbernal@gmail.com>
2017-12-09 18:53:44 -0500

    PrintService implemented

--- Parqueadero/Services/PrintService.cs
+++ Parqueadero/Services/PrintService.cs
@@ -2,6 +2,9 @@
 using System.Threading.Tasks;
 using System.Net.Http;
 using Parqueadero.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Text;
 using Parqueadero.Helpers;
 
 namespace Parqueadero.Services
@@ -10,17 +13,36 @@ namespace Parqueadero.Services
     {
         public async Task<bool> PrintCheckIn(VehicleRecord vehicle)
         {
-            using (HttpClient client = new HttpClient())
-            {
-                client.Timeout = new TimeSpan(0, 0, 10);
-            }
-
-            return true;
+            return await Print(vehicle);
         }
 
         public async Task<bool> PrintCheckOut(VehicleRecord vehicle)
         {
-            return true;
+            return await Print(vehicle);
+        }
+
+        private async Task<bool> Print(VehicleRecord vehicle)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.Timeout = new TimeSpan(0, 0, 10);
+                    var resolver = new DefaultContractResolver() { NamingStrategy = new SnakeCaseNamingStrategy() };
+                    var jsonSettings = new JsonSerializerSettings() { ContractResolver = resolver };
+                    string content = JsonConvert.SerializeObject(vehicle, jsonSettings);
+                    StringContent body = new StringContent(content, Encoding.UTF8, "application/json");
+
+                    var result = await client.PostAsync(Constants.PrinterAddress, body);
+                    var data = await result.Content.ReadAsStringAsync();
+
+                    return result.IsSuccessStatusCode;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
