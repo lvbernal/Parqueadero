1d3def09d78a6e151cb756c27921629553ea7e7c
Leonardo <lvbernal@gmail.com>
2017-12-21 09:29:38 -0500

    CanExecute management added to Main

--- Parqueadero/ViewModels/MainViewModel.cs
+++ Parqueadero/ViewModels/MainViewModel.cs
@@ -55,13 +55,27 @@ namespace Parqueadero.ViewModels
             }
         }
 
+        private bool _busy;
+        public bool Busy
+        {
+            get { return _busy; }
+            set
+            {
+                _busy = value;
+                NotifyPropertyChanged();
+                DoCheckInCommand.ChangeCanExecute();
+                DoCheckOutCommand.ChangeCanExecute();
+                ShowSummaryCommand.ChangeCanExecute();
+            }
+        }
+
         private bool loadingVehicles = false;
 
         public MainViewModel()
         {
-            DoCheckInCommand = new Command(DoCheckIn);
-            DoCheckOutCommand = new Command(DoCheckOut);
-            ShowSummaryCommand = new Command(ShowSummary);
+            DoCheckInCommand = new Command(DoCheckIn, () => !Busy);
+            DoCheckOutCommand = new Command(DoCheckOut, () => !Busy);
+            ShowSummaryCommand = new Command(ShowSummary, () => !Busy);
 
             Parking = new ParkingLot();
             Data = new DataService(Parking);
@@ -72,25 +86,31 @@ namespace Parqueadero.ViewModels
         public Command DoCheckInCommand { get; }
         public async void DoCheckIn()
         {
+            Busy = true;
             SyncVehicles();
             CheckIn = new CheckInViewModel() { Parking = Parking, Data = Data };
             await Application.Current.MainPage.Navigation.PushAsync(new CheckInPage());
+            Busy = false;
         }
 
         public Command DoCheckOutCommand { get; }
         public async void DoCheckOut()
         {
+            Busy = true;
             SyncVehicles();
             CheckOut = new CheckOutViewModel() { Parking = Parking, Data = Data };
             await Application.Current.MainPage.Navigation.PushAsync(new CheckOutPage());
+            Busy = false;
         }
 
         public Command ShowSummaryCommand { get; }
         public async void ShowSummary()
         {
+            Busy = true;
             SyncVehicles();
             Summary = new SummaryViewModel() { Parking = Parking, Data = Data };
             await Application.Current.MainPage.Navigation.PushAsync(new SummaryPage());
+            Busy = false;
         }
 
         public async Task SyncVehicles()
