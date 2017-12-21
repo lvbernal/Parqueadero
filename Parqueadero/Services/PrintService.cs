ea05cbed0c62c8030c10458d2c098c7f939d6710
Leonardo <lvbernal@gmail.com>
2017-12-20 22:04:38 -0500

    printer timeout reduced

--- Parqueadero/Services/PrintService.cs
+++ Parqueadero/Services/PrintService.cs
@@ -32,7 +32,7 @@ namespace Parqueadero.Services
             {
                 using (HttpClient client = new HttpClient())
                 {
-                    client.Timeout = new TimeSpan(0, 0, 10);
+                    client.Timeout = new TimeSpan(0, 0, 5);
                     var resolver = new DefaultContractResolver() { NamingStrategy = new SnakeCaseNamingStrategy() };
                     var jsonSettings = new JsonSerializerSettings() { ContractResolver = resolver };
                     string content = JsonConvert.SerializeObject(vehicle, jsonSettings);
